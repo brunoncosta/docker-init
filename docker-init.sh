9ea114449757d6ef93e4cc62d8b423601aca0515
Bruno Costa <brunoncosta@brunoncosta.home>
2020-04-27 16:41:30 +0100

    Commented nginx & added apache

--- docker-init.sh
+++ docker-init.sh
@@ -12,14 +12,17 @@ echo "Project Directory: $projectDirectory"
 echo "Project Name: $projectName"
 echo "-----"
 
-echo "nginx UUID:"
-sudo docker run -d -p 80:80 -v $projectDirectory:/usr/share/nginx/html --hostname $projectName nginx
+echo "php 7.4.5-apache UUID:"
+sudo docker run -d -p 80:80 -v $projectDirectory:/var/www/html --name php-$projectName php:7.4.5-apache
 
-echo "mysql UUID:"
+# echo "nginx lasted UUID:"
+# sudo docker run -d -p 80:80 -v $projectDirectory:/usr/share/nginx/html --hostname $projectName nginx
+
+echo "mysql lasted UUID:"
 sudo docker run -d -v ~/docker-mysql:/var/lib/mysql --name mysql-$projectName -e MYSQL_ROOT_PASSWORD="password" mysql
 
-echo "phpmyadmin UUID:"
-sudo docker run --name phpmyadmin-$projectName -d --link mysql-$projectName:db -p 8080:80 phpmyadmin/phpmyadmin
+echo "phpmyadmin lasted UUID:"
+sudo docker run -d -p 8080:80 --name phpmyadmin-$projectName --link mysql-$projectName:db phpmyadmin/phpmyadmin
 
 sudo /bin/sh -c 'echo "127.0.0.1 '$projectHost' localhost" > /etc/hosts'
 
